4e6cec9d3cc0d1ebdc6ee46e8bdd7adf7a4dc772
Elliott Brown <elliott.brown@ellersoft.com>
2019-02-19 11:46:15 -0500

    Add `Object.replace` for situations where one wants to use point-free code to make an object (regardless of what it is) a default.

--- FSharpExtensions/Primitives/Object.fs
+++ FSharpExtensions/Primitives/Object.fs
@@ -1,3 +1,6 @@
 ﻿module Object
 /// Equivalent to object.ToString.
 let toStr o = o.ToString()
+
+/// Effectively transforms `'a -> 'b`, where `'b` is a static default value.
+let replace (def : 'b) (_ : 'a) = def
