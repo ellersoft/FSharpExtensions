912e47eb26b42c87179610d8d5deb994b58e5d3d
Elliott Brown <ebrown8534@gmail.com>
2021-03-19 09:25:02 -0400

    Add date format constants and make `parseExact` and `toString` callable from the outside

--- FSharpExtensions/Primitives/Date.fs
+++ FSharpExtensions/Primitives/Date.fs
@@ -1,30 +1,69 @@
 ﻿module Date
 open System
 
+module Formats =
+    /// The M/d/yy format
+    let MDYY = "M/d/yy"
+    /// The MM/dd/yy format
+    let MMDDYY = "MM/dd/yy"
+    /// The M/d/yyyy format
+    let MDYYYY = "M/d/yyyy"
+    /// The MM/dd/yyyy format
+    let MMDDYYYY = "MM/dd/yyyy"
+    /// The d/M/yy format
+    let DMYY = "d/M/yy"
+    /// The dd/MM/yy format
+    let DDMMYY = "dd/MM/yy"
+    /// The d/M/yyyy format
+    let DMYYYY = "d/M/yyyy"
+    /// The dd/MM/yyyy format
+    let DDMMYYYY = "dd/MM/yyyy"
+    /// The yyyy-MM-dd format
+    let YYYYMMDD = "yyyy-MM-dd"
+    
+
 module From = 
-    let private parseExact (f : string) (s : string) =
+    let parseExact (f : string) (s : string) =
         (s, f, null) |> DateTime.ParseExact
         
-    let MDYY = parseExact "M/d/yy"
-    let MMDDYY = parseExact "MM/dd/yy"
-    let MDYYYY = parseExact "M/d/yyyy"
-    let MMDDYYYY = parseExact "MM/dd/yyyy"
-    let DMYY = parseExact "d/M/yy"
-    let DDMMYY = parseExact "dd/MM/yy"
-    let DMYYYY = parseExact "d/M/yyyy"
-    let DDMMYYYY = parseExact "dd/MM/yyyy"
-    let YYYYMMDD = parseExact "yyyy-MM-dd"
+    /// Parse from the M/d/yy format
+    let MDYY = parseExact Formats.MDYY
+    /// Parse from the MM/dd/yy format
+    let MMDDYY = parseExact Formats.MMDDYY
+    /// Parse from the M/d/yyyy format
+    let MDYYYY = parseExact Formats.MDYYYY
+    /// Parse from the MM/dd/yyyy format
+    let MMDDYYYY = parseExact Formats.MMDDYYYY
+    /// Parse from the d/M/yy format
+    let DMYY = parseExact Formats.DMYY
+    /// Parse from the dd/MM/yy format
+    let DDMMYY = parseExact Formats.DDMMYY
+    /// Parse from the d/M/yyyy format
+    let DMYYYY = parseExact Formats.DMYYYY
+    /// Parse from the dd/MM/yyyy format
+    let DDMMYYYY = parseExact Formats.DDMMYYYY
+    /// Parse from the yyyy-MM-dd format
+    let YYYYMMDD = parseExact Formats.YYYYMMDD
 
 module To =
-    let private toString (s : string) (d : DateTime) =
+    let toString (s : string) (d : DateTime) =
         s |> d.ToString
         
-    let MDYY = toString "M/d/yy"
-    let MMDDYY = toString "MM/dd/yy"
-    let MDYYYY = toString "M/d/yyyy"
-    let MMDDYYYY = toString "MM/dd/yyyy"
-    let DMYY = toString "d/M/yy"
-    let DDMMYY = toString "dd/MM/yy"
-    let DMYYYY = toString "d/M/yyyy"
-    let DDMMYYYY = toString "dd/MM/yyyy"
-    let YYYYMMDD = toString "yyyy-MM-dd"
+    /// Output to the M/d/yy format
+    let MDYY = toString Formats.MDYY
+    /// Parse from the MM/dd/yy format
+    let MMDDYY = toString Formats.MMDDYY
+    /// Parse from the M/d/yyyy format
+    let MDYYYY = toString Formats.MDYYYY
+    /// Parse from the MM/dd/yyyy format
+    let MMDDYYYY = toString Formats.MMDDYYYY
+    /// Parse from the d/M/yy format
+    let DMYY = toString Formats.DMYY
+    /// Parse from the dd/MM/yy format
+    let DDMMYY = toString Formats.DDMMYY
+    /// Parse from the d/M/yyyy format
+    let DMYYYY = toString Formats.DMYYYY
+    /// Parse from the dd/MM/yyyy format
+    let DDMMYYYY = toString Formats.DDMMYYYY
+    /// Parse from the yyyy-MM-dd format
+    let YYYYMMDD = toString Formats.YYYYMMDD
